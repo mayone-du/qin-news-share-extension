2bc663c8be62cc2df8fc1f22ce1a1c60c4501c4f
mayone-du <cocomayo1201@icloud.com>
2021-07-26 12:22:43 +0900

    Update UI

--- src/App.tsx
+++ src/App.tsx
@@ -34,45 +34,51 @@ export const App = () => {
         <button type="submit">シェア</button>
       </form>
 
-      <h2>今日のニュース</h2>
-      {/* loading */}
-      {news === undefined && (
-        <div className="bl_loading">
-          <svg
-            className="mr-3 -ml-1 w-5 h-5 text-black animate-spin"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            ></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-          </svg>
-        </div>
-      )}
-      {/* ニュース一覧 */}
-      <ul className="bl_newsList">
-        {news?.data.todayNews.edges.map((news, index) => {
-          return (
-            <li className="bl_newsList_news" key={index}>
-              <a href={news.node.url} target="_blank" rel="noopener noreferrer">
-                <span className="title">{news.node.title}</span>
-                <span className="summary">{news.node.summary}</span>
-              </a>
-            </li>
-          );
-        })}
-      </ul>
+      <section>
+        <h2>今日のニュース</h2>
+        {/* loading */}
+        {news === undefined && (
+          <div className="bl_loading">
+            <svg
+              className="mr-3 -ml-1 w-5 h-5 text-black animate-spin"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          </div>
+        )}
+        {/* ニュース一覧 */}
+        <ul className="bl_newsList">
+          {news?.data.todayNews.edges.map((news, index) => {
+            return (
+              <li className="bl_newsList_news" key={index}>
+                <a
+                  href={news.node.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span className="title">{news.node.title}</span>
+                  <span className="summary">{news.node.summary}</span>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     </div>
   );
 };
